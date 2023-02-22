641d7fc0dee8b1cd0cd2e0d4dc9e8e153e651291
Nebojsa Markovic <nebojsa91markovic@gmail.com>
2023-02-22 14:57:23 +0100

    added context - them context i song context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SongList from "./components/SongList";
+import ThemeToggle from "./components/ThemeToggle";
+import SongContextProvider from "./context/SongContext";
+import ThemeContextProvider from "./context/ThemeContext";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>useContext</h1>
+      <ThemeContextProvider>
+        <ThemeToggle />
+
+        <SongContextProvider>
+          <SongList />
+        </SongContextProvider>
+      </ThemeContextProvider>
     </div>
   );
 }
